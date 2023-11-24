96b4b344b6fb4feab87e60991519cc2956956b59
TOSHKA-711 <aliovich711@gmail.com>
2023-11-24 20:05:40 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,123 @@
+(function($) {
+
+	"use strict";
+
+
+  // Form
+	var contactForm = function() {
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					subject: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 5
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					subject: "Please enter your subject",
+					email: "Please enter a valid email address",
+					message: "Please enter a message"
+				},
+				/* submit via ajax */
+				
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/sendEmail.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeIn();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+
+		               	setTimeout(function(){
+				               $('#form-message-success').fadeOut();   
+		               	}, 8000);
+
+		               	setTimeout(function(){
+				               $submit.css('display', 'none').text(waitText);  
+		               	}, 1400);
+
+		               	setTimeout(function(){
+		               		$( '#contactForm' ).each(function(){
+											    this.reset();
+											});
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		} // end submitHandler
+
+			});
+		}
+	};
+	contactForm();
+
+})(jQuery);
+
+
+// edited slider
+
+
+
+
+const radioButtons = document.querySelectorAll('input[name="slider"]');
+const videos = document.querySelectorAll('video');
+let currentIndex = 0;
+const intervalDuration = 22000;
+const switchDuration = 22000; 
+
+function switchVideo() {
+    radioButtons[currentIndex].checked = false;
+    videos[currentIndex].autoplay = false;
+
+    currentIndex = (currentIndex + 1) % radioButtons.length;
+
+    radioButtons[currentIndex].checked = true;
+    videos[currentIndex].autoplay = true;
+}
+
+setInterval(switchVideo, intervalDuration);
+
+
+setTimeout(() => {
+    radioButtons[currentIndex].checked = false;
+    videos[currentIndex].autoplay = false;
+
+    currentIndex = (currentIndex + 1) % radioButtons.length;
+
+    radioButtons[currentIndex].checked = true;
+    videos[currentIndex].autoplay = true;
+}, switchDuration);
+
+
+// end edited slider
